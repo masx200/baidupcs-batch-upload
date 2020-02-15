bd0d077a56be975ccbc57af62cc95798873780ae
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-15 20:28:12 +0800

    Update uploadfile.js

--- uploadfile.js
+++ uploadfile.js
@@ -1,4 +1,3 @@
-const errormsg = "全部上传完毕, 总大小: 0B";
 const retrymsg = [
     "网络错误, http 响应错误,",
     `遇到错误, 远端服务器返回错误, 代码: 31352, 消息: commit superfile2 failed`,
@@ -7,7 +6,8 @@ const retrymsg = [
     "获取文件列表错误, 获取目录下的文件列表",
     "网络错误, Get",
     `上传文件错误: 上传状态过期, 请重新上传`,
-    `上传文件失败, 分片上传—合并分片文件:`
+    `上传文件失败, 分片上传—合并分片文件:`,
+    "全部上传完毕, 总大小: 0B"
 ];
 const successmsg = [
     "秒传成功, 保存到网盘路径:",
@@ -31,24 +31,22 @@ export async function upload(file, destination) {
     };
     console.log(JSON.stringify(记录日志, null, 4));
     /* 判断是否上传成功与失败 */
-    if (
-        !successmsg.some(m => stdout.includes(m)) &&
-        stdout.includes(errormsg)
-    ) {
-        if (retrymsg.some(msg => stdout.includes(msg))) {
-            console.warn(stdout);
-            console.warn("上传失败,5秒后重试:" + file);
-            return new Promise(res => {
-                setTimeout(() => {
-                    res(upload(file, destination));
-                }, 5000);
-            });
-        } else {
-            throw new Error(
-                "baidupcs-go:" + JSON.stringify({ stdout, stderr })
-            );
-        }
-    } else {
+    if (successmsg.some(m => stdout.includes(m))) {
         console.log("文件上传成功", file);
+        return;
     }
+
+    if (retrymsg.some(msg => stdout.includes(msg))) {
+        console.warn(stdout);
+        console.warn("上传失败,5秒后重试:" + file);
+        return new Promise(res => {
+            setTimeout(() => {
+                res(upload(file, destination));
+            }, 5000);
+        });
+    }
+
+    throw new Error(
+        "command failure! baidupcs-go:" + JSON.stringify({ stdout, stderr })
+    );
 }
