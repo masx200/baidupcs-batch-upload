c216740ada1a57b430e86710bda2d3f31b98c8fc
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-04 16:50:12 +0800

    Update cli.ts

--- lib/cli.ts
+++ lib/cli.ts
@@ -7,7 +7,7 @@ import process from "process";
 import { fileURLToPath } from "url";
 import { argsobj } from "./argsobj.js";
 import { start } from "./index.js";
-const { input, dest } = argsobj;
+const { input, dest,concurrent } = argsobj;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
