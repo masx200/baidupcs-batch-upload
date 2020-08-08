a9f0171e70a1d37f9acfac6bc16b5ad644c85fa8
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-08 12:21:59 +0800

    Update limiter.ts

--- lib/limiter.ts
+++ lib/limiter.ts
@@ -2,7 +2,7 @@ import AsyncLimiter from "@masx200/async-task-current-limiter";
 import { execFile } from "child_process";
 import { promisify } from "util";
 import { argsobj } from "./argsobj.js";
-const 同时并发的上传文件个数 = Number(argsobj["concurrent"]) || 12;
+const 同时并发的上传文件个数 = Number(argsobj["concurrent"]) || 15;
 const limiter = AsyncLimiter(同时并发的上传文件个数);
 /**
  * @param {any} data
