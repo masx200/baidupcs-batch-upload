4a4f31a5eb93b84a308821b3cb9b737a8f3e94c7
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-08 12:20:43 +0800

    Create execmeta.ts

---
+++ lib/execmeta.ts
@@ -0,0 +1,10 @@
+import { limitexec } from "./limiter.js";
+
+import { cmd } from "./index.js";
+
+
+export default function execmeta(remotefile: string) {
+
+    return limitexec(cmd, ["meta", remotefile,]);
+
+}
