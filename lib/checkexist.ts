c2d6b2c7d1e8c72153eec3a7492edcc1414de0d2
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-08 16:30:53 +0800

    Update checkexist.ts

--- lib/checkexist.ts
+++ lib/checkexist.ts
@@ -1,6 +1,13 @@
 export async function checkexist(remotefile){
 const  result = await execmeta(remotefile)
     const { stdout, stderr } = result
+
+const 记录日志={
+
+remotefile,
+stdout, stderr
+}
+console.log(JSON.stringify(记录日志,null,4))
 }
 
 
