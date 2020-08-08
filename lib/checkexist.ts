d144068c3ff3adb86afcd6b1e5b34c9fbf038017
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-08 16:37:19 +0800

    Update checkexist.ts

--- lib/checkexist.ts
+++ lib/checkexist.ts
@@ -1,5 +1,19 @@
-export async function checkexist(remotefile){
-const  result = await execmeta(remotefile)
+export async function checkexist(remotefile:string){
+const  result = await retry(
+        () => {
+
+execmeta(remotefile)
+
+},{
+
+
+times: 5,
+            onFailedAttempt: async (e) => {
+                console.warn(e);
+                console.warn("网络错误，3秒后重试");
+                await sleep(3000);
+            },
+})
     const { stdout, stderr } = result
 
 const 记录日志={
@@ -12,3 +26,5 @@ console.log(JSON.stringify(记录日志,null,4))
 
 
 import execmeta from "./execmeta.js"
+import pupkg from "@shanyue/promise-utils";
+const { retry, sleep } = pupkg;
