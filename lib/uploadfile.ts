4d3b5a6f5f6dcd1a3707eaa5182e6124725df84e
masx200 <masx200@qq.com>
2020-11-02 12:15:31 +0800

    在上传文件之前先检查文件是否存在于网盘中

--- lib/uploadfile.ts
+++ lib/uploadfile.ts
@@ -207,10 +207,35 @@ export async function uploadandcheck(
     file: string,
     destination: string
 ): Promise<void> {
-    await upload(file, destination);
-
     const inputbase = path.basename(file);
     const remotefile = posix.join(destination, inputbase);
+
+    if (Math.random() > 0.5) {
+        const starttime = Date.now();
+        const fileexist = await checkexist(remotefile);
+
+        if (fileexist) {
+            console.log(
+                ["检查网盘中存在此文件，跳过上传成功：", file, remotefile].join(
+                    "\n"
+                )
+            );
+            const endtime = Date.now();
+            const durtime = (endtime - starttime) / 1000;
+            console.log("用时" + durtime + "秒");
+            return;
+        } else {
+            console.warn(
+                [
+                    "检查网盘中不存在此文件，开始上传文件：",
+                    file,
+                    remotefile,
+                ].join("\n")
+            );
+        }
+    }
+    await upload(file, destination);
+
     const starttime = Date.now();
     const fileexist = await checkexist(remotefile);
     if (fileexist) {
